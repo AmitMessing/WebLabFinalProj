30fdea1bf1a8182581ae02eff1b8a9a3c904dd96
Amit Messing <amitmessing16@gmail.com>
2015-09-28 17:53:43 +0300

    Added design

--- FinalProject/Controllers/RegisterController.cs
+++ FinalProject/Controllers/RegisterController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FinalProject.Models;
 
 namespace FinalProject.Controllers
 {
     public class RegisterController : Controller
     {
+        private SiteContext ctx;
+
+        public RegisterController()
+        {
+            ctx = new SiteContext();
+        }
+
         // GET: Register
         public ActionResult Register()
         {
@@ -18,6 +26,23 @@ namespace FinalProject.Controllers
         public ActionResult ReturnToLoginView()
         {
             return RedirectToAction("Index","Login");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User user)
+        {
+            var duplicateUsers = ctx.Users.Select(x => x.UserName == user.UserName).ToList();
+            if (duplicateUsers.Any())
+            {
+                return Content("שם משתמש כבר קיים");
+            }
+            user.Id = Guid.NewGuid();
+            ctx.Users.Add(user);
+            ctx.SaveChanges();
+            //return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Login");
+
         }
     }
 }
